74ede5671beb7173336f536e61011b96a1d39711
CarlaRDGanier <carla.ganier@gmail.com>
2022-10-26 11:47:19 +0900

    First configuration

---
+++ app/src/main/java/com/example/applyv/FirstTimeUserActivity.java
@@ -0,0 +1,31 @@
+package com.example.applyv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstTimeUserActivity extends AppCompatActivity {
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_time_user);
+
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
