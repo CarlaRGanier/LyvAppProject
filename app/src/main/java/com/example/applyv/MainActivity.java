2fe9fd99ec34809128586e177ad96bf1e3b06d38
CarlaRDGanier <carla.ganier@gmail.com>
2022-12-21 17:06:59 +0900

    Allergies Tracker: fix problems (2)

--- app/src/main/java/com/example/applyv/MainActivity.java
+++ app/src/main/java/com/example/applyv/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.applyv;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -8,9 +10,14 @@ import android.widget.Button;
 import com.example.applyv.databinding.ActivityMainBinding;
 import com.example.applyv.ui.api.ApiHelper;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -39,8 +46,9 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
@@ -54,11 +62,38 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        //API
         ApiHelper apiHelper = new ApiHelper();
         apiHelper.getFoodInfo();
 
+        //Creating the chart
         BarChart barChart = findViewById(R.id.barchart);
+        getData();
         BarDataSet barDataSet = new BarDataSet(barArrayList, "Allegies");
+        BarData barData = new BarData(barDataSet);
+        barChart.setData(barData);
+        //BarDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+        //barChart.setDescription().setEnable(True);
+    }
+
+    private void getData(){
+        barArrayList = new ArrayList<>();
+        //Egg
+        barArrayList.add(new BarEntry(2f, 0));
+        //Gluten
+        barArrayList.add(new BarEntry(3f, 4));
+        //Nuts
+        barArrayList.add(new BarEntry(4f, 0));
+        //Grain
+        barArrayList.add(new BarEntry(5f, 2));
+        //Seafood
+        barArrayList.add(new BarEntry(5f, 3));
+        //Shellfish
+        barArrayList.add(new BarEntry(5f, 3));
+
     }
 
     @Override
